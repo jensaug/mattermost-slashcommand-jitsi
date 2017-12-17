8b5ee804f674b2045285405dfca5b00e1c02e4bb
Jens X Augustsson <jens.augustsson@redpill-linpro.com>
2017-12-17 01:11:51 +0100

    Testing header vs query

--- src/main/java/com/redpilllinpro/mattermost/SlashcommandJitsi.java
+++ src/main/java/com/redpilllinpro/mattermost/SlashcommandJitsi.java
@@ -5,9 +5,14 @@
  */
 package com.redpilllinpro.mattermost;
 
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
 
 /**
  *
@@ -19,10 +24,13 @@ public class SlashcommandJitsi {
     @GET
     @Path("meet")
     @Produces("application/json")
-    public SlashcommandResponse jitsi() {
+    public Response jitsi(@HeaderParam("user-agent") String userAgent, @QueryParam("channel_name") String channelName1, @HeaderParam("channel_name") String channelName2) {
         SlashcommandResponse resp = new SlashcommandResponse();
-        resp.text = "Let's meet!";
-        return resp;
+        resp.text += "\n*query:* " + channelName1;
+        resp.text += "\n*header:* " + channelName2;
+        resp.text += "addUser is called, userAgent : " + userAgent;
+		return Response.status(200)
+			.entity(resp)
+			.build();
     }
-
 }
